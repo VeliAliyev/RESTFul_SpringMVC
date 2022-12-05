a2b50f92a35a88d8bd6e56f3fb36f37afa648f5b
VeliAliyev <veli_aliyev@outlook.com>
2022-12-05 13:09:29 +0300

    Customer bootstrap added

--- src/main/java/com/velialiyev/restful_springmvc/bootstrap/Bootstrap.java
+++ src/main/java/com/velialiyev/restful_springmvc/bootstrap/Bootstrap.java
@@ -1,7 +1,9 @@
 package com.velialiyev.restful_springmvc.bootstrap;
 
 import com.velialiyev.restful_springmvc.domain.Category;
+import com.velialiyev.restful_springmvc.domain.Customer;
 import com.velialiyev.restful_springmvc.repository.CategoryRepository;
+import com.velialiyev.restful_springmvc.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRespository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        loadCategories();
+        loadCustomers();
+    }
+
+    private void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -29,13 +38,30 @@ public class Bootstrap implements CommandLineRunner {
         Category nuts = new Category();
         nuts.setName("Nuts");
 
-        categoryRepository.save(fruits);
-        categoryRepository.save(dried);
-        categoryRepository.save(fresh);
-        categoryRepository.save(exotic);
-        categoryRepository.save(nuts);
+        categoryRespository.save(fruits);
+        categoryRespository.save(dried);
+        categoryRespository.save(fresh);
+        categoryRespository.save(exotic);
+        categoryRespository.save(nuts);
 
+        System.out.println("Categories Loaded: " + categoryRespository.count());
+    }
 
-        System.out.println("Data Loaded = " + categoryRepository.count() );
+    private void loadCustomers() {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstname("Michale");
+        customer1.setLastname("Weston");
+        customerRepository.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstname("Sam");
+        customer2.setLastname("Axe");
+
+        customerRepository.save(customer2);
+
+        System.out.println("Customers Loaded: " + customerRepository.count());
     }
 }
